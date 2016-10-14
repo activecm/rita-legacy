54ac7537535ff1649b7476fb10a163c9e951dbac
Benjamin LeBrun <blebrun@blebrun-VirtualBox>
2016-10-14 13:51:01 -0600

    Fixed cli error in rita.go

--- rita.go
+++ rita.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"os"
+//	Removing this because fails when building fork
+//	Remember to replace before pull request
+//	"github.com/ocmdev/rita/commands"
+	"github.com/bglebrun/rita/commands"
 
-	"github.com/ocmdev/rita/commands"
-
-	"github.com/urfave/cli"
+	"github.com/codegangsta/cli"
 )
 
 // Entry point of ai-hunt
