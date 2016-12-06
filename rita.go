f96bfdb3c76a0b0a5b53a42c9176090a59241158
Benjamin LeBrun <bl.lebrun@gmail.com>
2016-12-06 11:21:49 -0700

    Removed reminder in rita.go

--- rita.go
+++ rita.go
@@ -16,7 +16,7 @@ func main() {
 
 	// Change the version string with updates so that a quick help command will
 	// let the testers know what version of HT they're on
-	app.Version = "0.9.1 Beta [REMEMEBER TO REPLACE /bglebrun/rita WITH /ocmdev/rita BEFORE PULL REQUEST]"
+	app.Version = "0.9.1 Beta"
 
 	// Define commands used with this application
 	app.Commands = commands.Commands()
