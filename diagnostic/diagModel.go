28a8b6685f4c404835dcaeb9630160e207a14553
Benjamin LeBrun <bl.lebrun@gmail.com>
2017-01-26 17:24:51 -0700

    Removed stray files from other work branch, should be good to go now

--- diagnostic/diagModel.go
+++
@@ -1,9 +0,0 @@
-package diagnostic
-
-import "time"
-
-type diagModel struct {
-	ErrorMessage string      `json:"errorMessage" bson:"errorMessage"`
-	Time         time.Time   `json:"time" bson:"time"`
-	FailedLog    interface{} `json:"failedLog" bson:"failedLog"`
-}
