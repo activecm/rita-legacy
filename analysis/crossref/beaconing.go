0d6ea83b86b38cf14614abd214aaddb87d5e6e02
logan lembke <logan@blackhillsinfosec.com>
2017-02-02 17:32:32 -0700

    Add beaconing selector

--- analysis/crossref/beaconing.go
+++ analysis/crossref/beaconing.go
@@ -1,6 +1,10 @@
 package crossref
 
-import "github.com/ocmdev/rita/database"
+import (
+	"github.com/ocmdev/rita/analysis/TBD"
+	"github.com/ocmdev/rita/database"
+	dataTBD "github.com/ocmdev/rita/datatypes/TBD"
+)
 
 type (
 	BeaconingSelector struct{}
@@ -11,9 +15,26 @@ func (s BeaconingSelector) GetName() string {
 }
 
 func (s BeaconingSelector) Select(res *database.Resources) (<-chan string, <-chan string) {
+	// make channels to return
 	internalHosts := make(chan string)
 	externalHosts := make(chan string)
+	// run the read code async and return the channels immediately
 	go func() {
+		iter := TBD.GetTBDResultsView(res, .7)
+
+		var data dataTBD.TBDAnalysisView
+		for iter.Next(&data) {
+			if data.LocalSrc {
+				internalHosts <- data.Src
+			} else {
+				externalHosts <- data.Src
+			}
+			if data.LocalDst {
+				internalHosts <- data.Dst
+			} else {
+				externalHosts <- data.Dst
+			}
+		}
 		close(internalHosts)
 		close(externalHosts)
 	}()
