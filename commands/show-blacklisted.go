1ea49e18287503ad8d26f6196bb0e4b88ae342cc
logan lembke <logan@blackhillsinfosec.com>
2017-02-17 00:12:51 -0700

    Fix blacklisted output

--- commands/show-blacklisted.go
+++ commands/show-blacklisted.go
@@ -3,35 +3,24 @@ package commands
 import (
 	"fmt"
 	"os"
-	"sort"
+	"strconv"
+	"strings"
 
 	"gopkg.in/mgo.v2/bson"
 
 	"github.com/alecthomas/template"
 	"github.com/ocmdev/rita/database"
+	"github.com/olekukonko/tablewriter"
 	"github.com/urfave/cli"
 )
+
+var sourcesFlag bool
 
 type blresult struct {
 	Host    string `bson:"host"`
 	Score   int    `bson:"count"`
-	Sources string
-}
-
-var globalSourcesFlag bool
-
-type blresults []blresult
-
-func (slice blresults) Len() int {
-	return len(slice)
-}
-
-func (slice blresults) Less(i, j int) bool {
-	return slice[j].Score < slice[i].Score
-}
-
-func (slice blresults) Swap(i, j int) {
-	slice[i], slice[j] = slice[j], slice[i]
+	IsUrl   bool   `bson:"is_url"`
+	Sources []string
 }
 
 func init() {
@@ -44,7 +33,7 @@ func init() {
 			cli.BoolFlag{
 				Name:        "sources, s",
 				Usage:       "Show sources with results",
-				Destination: &globalSourcesFlag,
+				Destination: &sourcesFlag,
 			},
 		},
 		Action: showBlacklisted,
@@ -58,13 +47,85 @@ func showBlacklisted(c *cli.Context) error {
 		return cli.NewExitError("Specify a database with -d", -1)
 	}
 
-	if humanreadable {
-		return showBlacklistedHuman(c)
+	res := database.InitResources("")
+	res.DB.SelectDB(c.String("database"))
+
+	var result blresult
+	var results []blresult
+
+	coll := res.DB.Session.DB(c.String("database")).C(res.System.BlacklistedConfig.BlacklistTable)
+	iter := coll.Find(nil).Sort("-count").Iter()
+
+	for iter.Next(&result) {
+		if sourcesFlag {
+			findBLSources(res, c.String("database"), &result)
+		}
+		results = append(results, result)
 	}
 
+	if humanreadable {
+		return showBlacklistedHuman(results)
+	}
+	return showBlacklistedCsv(results)
+}
+
+func findBLSources(res *database.Resources, db string, result *blresult) {
+	if result.IsUrl {
+		hostnames := res.DB.Session.DB(db).C(res.System.UrlsConfig.HostnamesTable)
+		var destIPs struct {
+			IPs []string `bson:"ips"`
+		}
+		hostnames.Find(bson.M{"host": result.Host}).One(&destIPs)
+		for _, destIP := range destIPs.IPs {
+			result.Sources = append(result.Sources, getConnSourceFromDest(res, db, destIP)...)
+		}
+	} else {
+		result.Sources = getConnSourceFromDest(res, db, result.Host)
+	}
+}
+
+func getConnSourceFromDest(res *database.Resources, db string, ip string) []string {
+	cons := res.DB.Session.DB(db).C(res.System.StructureConfig.UniqueConnTable)
+	srcIter := cons.Find(bson.M{"dst": ip}).Iter()
+
+	var srcStruct struct {
+		Src string `bson:"src"`
+	}
+	var sources []string
+
+	for srcIter.Next(&srcStruct) {
+		sources = append(sources, srcStruct.Src)
+	}
+	return sources
+}
+
+// TODO: Convert this over to tablewriter
+// showBlacklisted prints all blacklisted for a given database
+func showBlacklistedHuman(results []blresult) error {
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetColWidth(100)
+	if sourcesFlag {
+		table.SetHeader([]string{"Host", "Score", "Sources"})
+		for _, result := range results {
+			table.Append([]string{
+				result.Host, strconv.Itoa(result.Score), strings.Join(result.Sources, ", "),
+			})
+		}
+	} else {
+		table.SetHeader([]string{"Host", "Score"})
+		for _, result := range results {
+			table.Append([]string{result.Host, strconv.Itoa(result.Score)})
+		}
+	}
+
+	table.Render()
+	return nil
+}
+
+func showBlacklistedCsv(results []blresult) error {
 	tmpl := "{{.Host}}," + `{{.Score}}`
-	if globalSourcesFlag {
-		tmpl += ", {{.Sources}}\n"
+	if sourcesFlag {
+		tmpl += ",{{range $idx, $src := .Sources}}{{if $idx}} {{end}}{{ $src }}{{end}}\n"
 	} else {
 		tmpl += "\n"
 	}
@@ -73,77 +134,12 @@ func showBlacklisted(c *cli.Context) error {
 		panic(err)
 	}
 
-	res := database.InitResources("")
-	res.DB.SelectDB(c.String("database"))
-
-	var result blresult
-	var allResults blresults
-
-	coll := res.DB.Session.DB(c.String("database")).C(res.System.BlacklistedConfig.BlacklistTable)
-	iter := coll.Find(nil).Iter()
-
-	for iter.Next(&result) {
-		if globalSourcesFlag {
-			result.Sources = ""
-			cons := res.DB.Session.DB(c.String("database")).C(res.System.StructureConfig.ConnTable)
-			siter := cons.Find(bson.M{"id_resp_h": result.Host}).Iter()
-
-			var srcStruct struct {
-				Src string `bson:"id_origin_h"`
-			}
-
-			for siter.Next(&srcStruct) {
-				result.Sources += srcStruct.Src + "; "
-			}
-		}
-		allResults = append(allResults, result)
-	}
-
-	sort.Sort(allResults)
-
-	for _, result := range allResults {
+	for _, result := range results {
 		err := out.Execute(os.Stdout, result)
 		if err != nil {
 			fmt.Fprintf(os.Stdout, "ERROR: Template failure: %s\n", err.Error())
 		}
 	}
-	return nil
-}
 
-// TODO: Convert this over to tablewriter
-// showBlacklisted prints all blacklisted for a given database
-func showBlacklistedHuman(c *cli.Context) error {
-
-	cols := "            host\tscore\n"
-	cols += "----------------\t-----\n"
-	tmpl := "{{.Host}}\t" + `{{.Score | printf "%5d"}}` + "\n"
-	out, err := template.New("bl").Parse(tmpl)
-	if err != nil {
-		panic(err)
-	}
-
-	res := database.InitResources("")
-	res.DB.SelectDB(c.String("database"))
-
-	var result blresult
-	var allResults blresults
-
-	coll := res.DB.Session.DB(c.String("database")).C(res.System.BlacklistedConfig.BlacklistTable)
-	iter := coll.Find(nil).Iter()
-
-	fmt.Printf(cols)
-	for iter.Next(&result) {
-		result.Host = padAddr(result.Host)
-		allResults = append(allResults, result)
-	}
-
-	sort.Sort(allResults)
-
-	for _, result := range allResults {
-		err := out.Execute(os.Stdout, result)
-		if err != nil {
-			fmt.Fprintf(os.Stdout, "ERROR: Template failure: %s\n", err.Error())
-		}
-	}
 	return nil
 }
