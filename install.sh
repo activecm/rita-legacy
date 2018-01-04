33bde86ceac95725b5e162b31c278189800d0d3d
Logan Lembke <logan@blackhillsinfosec.com>
2018-01-03 17:04:43 -0700

    Add security onion patch; support centos

--- install.sh
+++ install.sh
@@ -9,25 +9,36 @@ _SUCCESS="\e[92mSUCCESS\e[0m"
 
 #Error handling
 #Kill 0 to kill subshells as well
-trap "printf '\n[!] Installation $_FAILED!\n'; kill 0" ERR INT
+__err() {
+	printf "\n[!] Installation $_FAILED!\n"
+	kill 0
+}
+trap __err ERR INT
 set -o errexit
 set -o errtrace
 set -o pipefail
 
+# Fix $HOME for users under standard sudo
+if [ ! -z ${SUDO_USER+x} ]; then
+	HOME="$( getent passwd $SUDO_USER | cut -d: -f6 )"
+fi
+
 # Make sure to source the latest .bashrc
 # Hack the PS1 variable to get around ubuntu .bashrc
-OLD_PS1=$PS1
+_OLD_PS1=$PS1
 PS1=" "
 # Hack the interactive flag to get around other .bashrc's
 set -i
+# Make sure weirdness doesn't happen with autocomplete/ etc
+set -o posix
 
 source $HOME/.bashrc
 
 # Clean up our hacks
+set +o posix
 set +i
-PS1=$OLD_PS1
-unset OLD_PS1
-
+PS1=$_OLD_PS1
+unset _OLD_PS1
 
 __help() {
 	__title
@@ -43,13 +54,13 @@ Options:
 HEREDOC
 }
 
-__prep() {
+__explain() {
 	cat <<HEREDOC
 This script will:
 
 1) Download and install Bro IDS, Go, and MongoDB.
 
-2) Set up a Go development enviornment in order to 'go get'
+2) Set up a Go development environment in order to 'go get'
 and 'build' RITA. This requires us to create a directory "go"
 in your home folder and add new PATH and GOPATH entries
 to your .bashrc.
@@ -105,70 +116,87 @@ __uninstall() {
 	rm -rf $HOME/.rita
 }
 
-__install() {
-
-	# Check if RITA is already installed, if so ask if this is a re-install
-	if [ ! -z $(command -v rita) ] ||
-	[ -d $HOME/.rita ]
-	then
-		printf "[+] RITA is already installed.\n"
-		read -p "[-] Would you like to erase it and re-install? [y/n] " -r
-		if [[ $REPLY =~ ^[Yy]$ ]]
-		then
-			__uninstall
-		else
-			exit -1
-		fi
+__setPkgMgr() {
+	# _PKG_MGR = 1: APT: Ubuntu 14.04, 16.04 and Security Onion (Debian)
+	# _PKG_MGR = 2: YUM: CentOS (Old RHEL Derivatives)
+	# _PKG_MGR = 3: Unsupported
+	_PKG_MGR=3
+	_PKG_INSTALL=""
+	if [ -x /usr/bin/apt-get ];	then
+		_PKG_MGR=1
+		_PKG_INSTALL="apt-get -qq install -y"
+	elif [ -x /usr/bin/yum ];	then
+		_PKG_MGR=2
+		_PKG_INSTALL="yum -y -q install"
 	fi
-
-	__prep
-
-	# Install installation dependencies
-	apt-get update > /dev/null 2>&1 & __load "[+] Updating apt"
-
-	#Extracting templates from packages made it through...
-	apt-get install -y git wget make realpath lsb-release > /dev/null 2>&1 & \
-	__load "[+] Installing git, wget, make, realpath, and lsb-release"
-
-  # Install Bro IDS
-	printf "[+] Checking if Bro IDS is installed... "
-
-	if [ $(dpkg-query -W -f='${Status}' bro 2>/dev/null | grep -c "ok installed") -eq 0 ] &&
-	[ $(dpkg-query -W -f='${Status}' securityonion-bro 2>/dev/null | grep -c "ok installed") -eq 0 ]
-	then
-		printf "\n"
-		(
-			echo "deb http://download.opensuse.org/repositories/network:/bro/xUbuntu_$(lsb_release -rs)/ /" >> /etc/apt/sources.list.d/bro.list
-			wget -nv --quiet "http://download.opensuse.org/repositories/network:bro/xUbuntu_$(lsb_release -rs)/Release.key" -O Release.key
-			apt-key add - < Release.key > /dev/null 2>&1
-			rm Release.key
-			apt-get update > /dev/null 2>&1
-			apt-get install -y bro broctl> /dev/null 2>&1
-		) & __load "\t[+] Installing Bro IDS"
-	else
-		printf "$_SUCCESS\n"
+	if [ $_PKG_MGR -eq 3 ]; then
+		echo "Unsupported package manager"
+		_err
 	fi
+}
 
-  # Install Go
-	printf "[+] Checking if Go is installed...\n"
+__setOS() {
+	_OS="$(lsb_release -is)"
+	if [ "$_OS" != "Ubuntu" -a "$_OS" != "CentOS" ]; then
+		echo "Unsupported operating system"
+		_err
+	fi
+}
 
-	# Check if go is not available in the path
-	if [ -z $(command -v go) ]
-	then
-		# Check if go is available in the standard location
-		if [ ! -e "/usr/local/go" ]
-		then
-			( # golang most recent update
-				wget -q https://storage.googleapis.com/golang/go1.8.3.linux-amd64.tar.gz
-				tar -zxf go1.8.3.linux-amd64.tar.gz -C /usr/local/
-				rm go1.8.3.linux-amd64.tar.gz
-				echo 'export PATH=$PATH:/usr/local/go/bin' >> $HOME/.bashrc
-			) &	__load "\t[+] Installing Go"
+__install_packages() {
+	while [ ! -z "$1" ]; do
+		local pkg="$1"
+		# Translation layer
+		# apt -> yum
+		if [ $_PKG_MGR -eq 2 ]; then
+			case "$pkg" in
+				"lsb-release")
+					pkg="redhat-lsb-core"
+					;;
+				realpath)
+					pkg="coreutils"
+					;;
+			esac
 		fi
+		eval $_PKG_INSTALL $pkg >/dev/null 2>&1
+		shift
+	done
+}
 
-		# Add go to the path
-		export PATH="$PATH:/usr/local/go/bin"
-	else
+__freshen_packages() {
+	if [ $_PKG_MGR -eq 1 ]; then   #apt
+		apt-get -qq update > /dev/null 2>&1
+	elif [ $_PKG_MGR -eq 2 ]; then #yum
+		yum -q makecache > /dev/null 2>&1
+	fi
+}
+
+__package_installed() {
+	#Returns true if the package is installed, false otherwise
+	if [ $_PKG_MGR -eq 1 ]; then # apt
+		dpkg-query -W -f='${Status}' "$1" 2>/dev/null | grep -q "ok installed"
+	elif [ $_PKG_MGR -eq 2 ]; then # yum and dnf
+		rpm -q "$1" >/dev/null
+	fi
+}
+
+__add_deb_repo() {
+	if [ ! -s "/etc/apt/sources.list.d/$2.list" ]; then
+		if [ ! -z "$3" ]; then
+			curl -s -L "$3" | apt-key add - > /dev/null 2>&1
+		fi
+		echo "$1" > "/etc/apt/sources.list.d/$2.list"
+		__freshen_packages
+	fi
+}
+
+__add_rpm_repo() {
+	yum-config-manager -q --add-repo=$1 > /dev/null 2>&1
+}
+
+__check_go_version() {
+	case `go version | awk '{print $3}'` in
+	go1|go1.2*|go1.3*|go1.4*|go1.5*|go1.6*|"")
 		echo -e "\e[93m\t[!] WARNING: Go has been detected on this system.\e[0m
 \tIf you installed Go with apt, make sure your Go installation is up
 \tto date with 'go version'. RITA has only been tested with golang
@@ -178,35 +206,123 @@ __install() {
 \tversion for you!
 "
 		sleep 10s
+		;;
+	esac
+}
+
+__install_go() {
+	# Check if go isn't available in the path
+	printf "[+] Checking if Go is installed...\n"
+	if [ ! $(command -v go) ];	then
+		if [ ! -x "/usr/local/go/bin/go" ]; then
+			(
+				curl -s -o /tmp/golang.tar.gz https://storage.googleapis.com/golang/go1.8.3.linux-amd64.tar.gz
+				tar -zxf /tmp/golang.tar.gz -C /usr/local/
+				rm /tmp/golang.tar.gz
+			) & __load "\t[+] Installing Go"
+		fi
+		printf "\t[+] Adding Go to the PATH...\n"
+		export PATH="$PATH:/usr/local/go/bin"
+		echo 'export PATH=$PATH:/usr/local/go/bin' >> $HOME/.bashrc
+	else
+		printf "\t[+] Go is installed...\n"
 	fi
 
 	# Check if the GOPATH isn't set
-	if [ -z ${GOPATH+x} ]
-	then
+	if [ -z ${GOPATH+x} ]; then
 		( # Set up the GOPATH
 			mkdir -p $HOME/go/{src,pkg,bin}
 			echo 'export GOPATH=$HOME/go' >> $HOME/.bashrc
 			echo 'export PATH=$PATH:$GOPATH/bin' >> $HOME/.bashrc
-		) & __load "[+] Configuring Go dev environment"
+		) & __load "\t[+] Configuring Go dev environment"
 		export GOPATH=$HOME/go
 		export PATH=$PATH:$GOPATH/bin
 	fi
+}
 
-	# Install MongoDB
-	apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 \
-	--recv 0C49F3730359A14518585931BC711F9BA15703C6 > /dev/null 2>&1 & \
-	__load "[+] Obtaining the package key for MongoDB"
+__install_bro() {
+	(
+		# security onion packages bro on their own
+		if ! __package_installed bro && ! __package_installed securityonion-bro; then
+			case "$_OS" in
+				Ubuntu)
+					__add_deb_repo "deb http://download.opensuse.org/repositories/network:/bro/xUbuntu_$(lsb_release -rs)/ /" \
+						"Bro" \
+						"http://download.opensuse.org/repositories/network:bro/xUbuntu_$(lsb_release -rs)/Release.key"
+					;;
+				CentOS)
+					__add_rpm_repo http://download.opensuse.org/repositories/network:bro/CentOS_7/network:bro.repo
+					;;
+			esac
+			__install_packages bro broctl
+		fi
+	) & __load "[+] Ensuring Bro IDS is installed"
 
-	echo "deb [ arch=$(dpkg --print-architecture) ] http://repo.mongodb.org/apt/ubuntu $(lsb_release -cs)/mongodb-org/3.4 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.4.list
+	if [ ! $(command -v bro) ];	then
+		printf "\t[+] Adding Bro to the PATH...\n"
+		echo 'export PATH=$PATH:/opt/bro/bin' >> $HOME/.bashrc
+		PATH=$PATH:/opt/bro/bin
+	fi
+}
 
-	apt-get update > /dev/null 2>&1 & __load "[+] Updating apt"
-	apt-get install -y mongodb-org > /dev/null 2>&1 & __load "[+] Installing MongoDB"
+__install_mongodb() {
+	case "$_OS" in
+		Ubuntu)
+			__add_deb_repo "deb [ arch=$(dpkg --print-architecture) ] http://repo.mongodb.org/apt/ubuntu $(lsb_release -cs)/mongodb-org/3.4 multiverse" \
+				"MongoDB" \
+				"https://www.mongodb.org/static/pgp/server-3.4.asc"
+			;;
+		CentOS)
+			if [ ! -s /etc/yum.repos.d/mongodb-org-3.4.repo ]; then
+				echo -e '[mongodb-org-3.4]\nname=MongoDB Repository\nbaseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.4/x86_64/\ngpgcheck=1\nenabled=1\ngpgkey=https://www.mongodb.org/static/pgp/server-3.4.asc' > /etc/yum.repos.d/mongodb-org-3.4.repo
+			fi
+			;;
+	esac
+	__install_packages mongodb-org
+}
+
+__install() {
+
+	# Check if RITA is already installed, if so ask if this is a re-install
+	if [ ! -z $(command -v rita) ] ||	[ -d $HOME/.rita ];	then
+		printf "[+] RITA is already installed.\n"
+		read -p "[-] Would you like to erase it and re-install? [y/n] " -r
+		if [[ $REPLY =~ ^[Yy]$ ]]
+		then
+			__uninstall
+			echo ""
+		else
+			exit 1
+		fi
+	fi
+
+	# Explain the scripts actions
+	__explain
+
+	# Figure out which package manager to use
+	__setPkgMgr
+
+	# Update package sources
+	__freshen_packages
+
+	# Install "the basics"
+	__install_packages git wget curl make coreutils realpath lsb-release & \
+		__load "[+] Ensuring git, wget, curl, make, coreutils, and lsb-release are installed"
+
+	# Determine the OS, needs lsb-release
+	__setOS
+
+	__install_bro
+
+  __install_go
+	__check_go_version
+
+	__install_mongodb & __load "[+] Installing MongoDB"
 
 	( # Build RITA
 		mkdir -p $GOPATH/src/github.com/ocmdev/rita
 		# Get the install script's directory in case it's run from elsewhere
-		RITASRC="$(dirname "$(realpath ${0})")"
-		cp -R $RITASRC/. $GOPATH/src/github.com/ocmdev/rita/
+		cp -R "$(dirname "$(realpath ${0})")/." $GOPATH/src/github.com/ocmdev/rita/
 		cd $GOPATH/src/github.com/ocmdev/rita
 		make install > /dev/null
 	)	& __load "[+] Installing RITA"
