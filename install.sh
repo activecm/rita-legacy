1b0497268f56c8bb4abe4c7398580d139ede67eb
Logan Lembke <logo2462@gmail.com>
2018-04-20 11:35:16 -0600

    Pull a statically linked RITA binary from github (#205)
    * Pull a statically linked RITA binary from github rather than installing go and building from source
    * Switch logic for config file replacement
    * Added release instructions
    * Fix installation instructions
    * Fix https://github.com/activecm/rita/issues/189

--- install.sh
+++ install.sh
@@ -33,38 +33,9 @@ set -o errexit
 set -o errtrace
 set -o pipefail
 
-
-# PERMISSIONS GADGET
-# The user must run the build process, but root must install
-# software. In order to make sure the appropriate users
-# take the right actions, we call sudo in the script itself.
-
-# Prevent user running sudo themselves
-if [ ! -z ${SUDO_USER+x} ]; then
-	printf "Please run the RITA installer without sudo.\n"
+if ! [ $(id -u) = 0 ]; then 
+	echo "You do not have permissions to install RITA!"
 	exit 1
-fi
-
-# Root is running the script without sudo
-if [ "$EUID" = "0" ]; then
-	_ELEVATE=""
-else
-	printf "$_IMPORTANT The RITA installer requires root privileges for some tasks. \n"
-	printf "$_IMPORTANT \"sudo\" will be used when necessary. \n"
-	_SUDO="$(type -fp sudo)"
-	if [ -z $_SUDO ]; then
-		printf "\"sudo\" was not found on the system. Please log in as root \n"
-		printf "before running the installer, or install \"sudo\". \n"
-		exit 1
-	fi
-	$_SUDO -v
-	if [ $? -ne 0 ]; then
-		printf "The installer was unable to elevate privileges using \"sudo\". \n"
-		printf "Please make sure your account has \"sudo\" privileges. \n"
-	fi
-	# _ELEVATE is separate from _SUDO since environment variables may need
-	# to be passed
-	_ELEVATE="$_SUDO"
 fi
 
 # ENTRYPOINT
@@ -125,6 +96,7 @@ __entry() {
 		if [[ ! $REPLY =~ ^[Yy]$ ]]; then
 			exit 0
 		fi
+		_REINSTALL_RITA="true"
 	fi
 
 	__install
@@ -145,7 +117,6 @@ __install() {
 	# Get system information
 	__gather_OS
 	__gather_bro
-	__gather_go
 	__gather_mongo
 
 	# Explain the installer's actions
@@ -163,35 +134,6 @@ __install() {
 		fi
 	fi
 
-	# Always install Go
-	if [ "$_GO_OUT_OF_DATE" = "true" ]; then
-		printf "$_IMPORTANT WARNING: An old version of Go has been detected on this system. \n"
-		printf "$_IMPORTANT RITA has only been tested with Go >= 1.7. Check if the installed \n"
-		printf "$_IMPORTANT version of Go is up to date with 'go version'. If it is out of date \n"
-		printf "$_IMPORTANT you may remove the old version of Go and let this installer install \n"
-		printf "$_IMPORTANT a more recente version. \n"
-		sleep 10s
-	fi
-
-	if [ "$_GO_INSTALLED" = "false" ]; then
-		__load "$_ITEM Installing Go" __install_go
-	else
-		printf "$_ITEM Go is already installed \n"
-	fi
-
-	if [ "$_GO_IN_PATH" = "false" ]; then
-		__add_go_to_path
-	fi
-
-	if [ "$_GOPATH_EXISTS" = "false" ]; then
-		__create_go_path
-	else
-		printf "$_SUBITEM Found GOPATH at $GOPATH \n"
-		# Add the bin folder of the $GOPATH
-		# It may already be in the path, but oh well, better to be safe than sorry
-		export PATH=$PATH:$GOPATH/bin
-	fi
-
 	if [ $_INSTALL_MONGO = "true" ]; then
 		if [ $_MONGO_INSTALLED = "false" ]; then
 			__load "$_ITEM Installing MongoDB" __install_mongodb
@@ -200,7 +142,12 @@ __install() {
 		fi
 	fi
 
-	__load "$_ITEM Installing RITA" __build_rita && __install_rita
+	__load "$_ITEM Installing RITA" __install_rita
+	if [ "$_REINSTALL_RITA" = "true" ]; then
+		printf "$_IMPORTANT $_RITA_CONFIG_FILE may need to be updated for this version of RITA. \n"
+		printf "$_IMPORTANT A default config file has been created at $_RITA_REINSTALL_CONFIG_FILE. \n"
+		printf "$_IMPORTANT \"rita test-config\" may be used to troubleshoot configuration issues. \n \n"
+	fi
 
 	# Ubuntu 14.04 uses Upstart for init
         _START_MONGO="sudo systemctl start mongod"
@@ -210,12 +157,12 @@ __install() {
 		_STOP_MONGO="sudo service mongod stop"
 	fi
 
-	printf "$_IMPORTANT To finish the installtion, reload the system profile and \n"
-	printf "$_IMPORTANT user profile with 'source /etc/profile' and 'source ~/.profile'. \n"
-	printf "$_IMPORTANT Additionally, you may want to configure Bro and run 'sudo broctl deploy'. \n"
-	printf "$_IMPORTANT Finally, start MongoDB with '$_START_MONGO'. You can \n"
-	printf "$_IMPORTANT access the MongoDB shell with 'mongo'. If, at any time, you need \n"
-	printf "$_IMPORTANT to stop MongoDB, run '$_STOP_MONGO'. \n"
+	printf "$_IMPORTANT To finish the installation, reload the system profile with \n"
+	printf "$_IMPORTANT 'source /etc/profile'. Additionally, you may want to configure Bro \n"
+	printf "$_IMPORTANT by running 'sudo broctl deploy'. Finally, start MongoDB with \n"
+	printf "$_IMPORTANT '$_START_MONGO'. You can access the MongoDB shell with \n"
+	printf "$_IMPORTANT 'mongo'. If, at any time, you need to stop MongoDB, \n"
+	printf "$_IMPORTANT run '$_STOP_MONGO'. \n"
 
 	__title
 	printf "Thank you for installing RITA! Happy hunting! \n"
@@ -241,18 +188,14 @@ __install_bro() {
 			;;
 		CentOS)
 			__add_rpm_repo http://download.opensuse.org/repositories/network:bro/CentOS_7/network:bro.repo
-			
 			# Workaround for https://github.com/activecm/rita/issues/189
-			curl -sSL http://download.opensuse.org/repositories/network%3A/bro/CentOS_7/x86_64/bro-2.5.3-1.1.x86_64.rpm -o /tmp/bro-2.5.3-1.1.x86_64.rpm
-			curl -sSL http://download.opensuse.org/repositories/network%3A/bro/CentOS_7/x86_64/bro-core-2.5.3-1.1.x86_64.rpm -o /tmp/bro-core-2.5.3-1.1.x86_64.rpm
-			curl -sSL http://download.opensuse.org/repositories/network%3A/bro/CentOS_7/x86_64/broctl-2.5.3-1.1.x86_64.rpm -o /tmp/broctl-2.5.3-1.1.x86_64.rpm
-			curl -sSL http://download.opensuse.org/repositories/network%3A/bro/CentOS_7/x86_64/libbroccoli-2.5.3-1.1.x86_64.rpm -o /tmp/libbroccoli-2.5.3-1.1.x86_64.rpm
-			$_ELEVATE yum -y -q localinstall /tmp/bro-2.5.3-1.1.x86_64.rpm /tmp/bro-core-2.5.3-1.1.x86_64.rpm /tmp/broctl-2.5.3-1.1.x86_64.rpm /tmp/libbroccoli-2.5.3-1.1.x86_64.rpm
-			# End workaround
+			# Replace the download.opensuse.org link with downloadcontent.opensuse.org link
+			# https://www.linuxquestions.org/questions/linux-general-1/yum-update-failed-because-of-timeout-4175625075/#post5828487
+			cat '/etc/yum.repos.d/network:bro.repo' | sed -e 's/download\.opensuse\.org/downloadcontent.opensuse.org/g' | tee '/etc/yum.repos.d/network:bro.repo.tmp' >/dev/null && mv -f '/etc/yum.repos.d/network:bro.repo.tmp' '/etc/yum.repos.d/network:bro.repo'
 			;;
 	esac
 	__install_packages bro broctl
-	$_ELEVATE chmod 2755 /opt/bro/logs
+	chmod 2755 /opt/bro/logs
 	_BRO_PKG_INSTALLED=true
 	_BRO_PATH="/opt/bro/bin"
 }
@@ -262,54 +205,11 @@ __add_bro_to_path() {
 	read
 	if [[ ! $REPLY =~ ^[Nn]$ ]]; then
 		printf "$_SUBIMPORTANT Adding Bro IDS to the path in $_BRO_PATH_SCRIPT \n"
-		echo "export PATH=\"\$PATH:$_BRO_PATH\"" | $_ELEVATE tee $_BRO_PATH_SCRIPT > /dev/null
+		echo "export PATH=\"\$PATH:$_BRO_PATH\"" | tee $_BRO_PATH_SCRIPT > /dev/null
 		_BRO_PATH_SCRIPT_INSTALLED=true
 		export PATH="$PATH:$_BRO_PATH"
 		_BRO_IN_PATH=true
 	fi
-}
-
-__install_go() {
-		curl -s -o /tmp/golang.tar.gz https://storage.googleapis.com/golang/go1.8.3.linux-amd64.tar.gz
-		$_ELEVATE tar -zxf /tmp/golang.tar.gz -C /usr/local/
-		rm /tmp/golang.tar.gz
-		_GO_INSTALLED_STD=true
-		_GO_INSTALLED=true
-		_GO_PATH="/usr/local/go/bin"
-}
-
-__add_go_to_path() {
-		printf "$_SUBIMPORTANT Adding Go to the path in $_GO_PATH_SCRIPT \n"
-		echo "export PATH=\"\$PATH:$_GO_PATH\"" | $_ELEVATE tee $_GO_PATH_SCRIPT > /dev/null
-		_GO_PATH_SCRIPT_INSTALLED=true
-		export PATH="$PATH:$_GO_PATH"
-		_GO_IN_PATH=true
-}
-
-__create_go_path() {
-	printf "$_SUBIMPORTANT Go requires a per-user workspace (GOPATH) in order to build software. \n"
-
-	printf "$_SUBQUESTION Select a GOPATH [$HOME/go]: "
-	read
-	if [ -n "$REPLY" ]; then
-		export GOPATH="$REPLY"
-	else
-		export GOPATH="$HOME/go"
-	fi
-
-	printf "$_SUBIMPORTANT Creating a GOPATH at $GOPATH \n"
-	mkdir -p "$GOPATH/"{src,pkg,bin}
-	_GOPATH_EXISTS=true
-
-	export PATH="$PATH:$GOPATH/bin"
-
-	printf "$_SUBIMPORTANT Adding your GOPATH to $_GOPATH_PATH_SCRIPT \n"
-	echo "export GOPATH=\"$GOPATH\"" > "$_GOPATH_PATH_SCRIPT"
-	echo "export PATH=\"\$PATH:\$GOPATH/bin\"" >> "$_GOPATH_PATH_SCRIPT"
-	_GOPATH_PATH_SCRIPT_INSTALLED=true
-
-	printf "$_SUBIMPORTANT Adding $_GOPATH_PATH_SCRIPT to $HOME/.profile \n"
-	echo "source \"$_GOPATH_PATH_SCRIPT\"" >> "$HOME/.profile"
 }
 
 __install_mongodb() {
@@ -321,7 +221,7 @@ __install_mongodb() {
 			;;
 		CentOS)
 			if [ ! -s /etc/yum.repos.d/mongodb-org-3.4.repo ]; then
-				echo -e '[mongodb-org-3.4]\nname=MongoDB Repository\nbaseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.4/x86_64/\ngpgcheck=1\nenabled=1\ngpgkey=https://www.mongodb.org/static/pgp/server-3.4.asc' | $_ELEVATE tee /etc/yum.repos.d/mongodb-org-3.4.repo > /dev/null
+				echo -e '[mongodb-org-3.4]\nname=MongoDB Repository\nbaseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.4/x86_64/\ngpgcheck=1\nenabled=1\ngpgkey=https://www.mongodb.org/static/pgp/server-3.4.asc' | tee /etc/yum.repos.d/mongodb-org-3.4.repo > /dev/null
 			fi
 			;;
 	esac
@@ -329,49 +229,48 @@ __install_mongodb() {
 	_MONGO_INSTALLED=true
 }
 
-__build_rita() {
-	curl -L -s -o "$GOPATH/bin/dep" https://github.com/golang/dep/releases/download/v0.3.2/dep-linux-amd64
-	chmod +x "$GOPATH/bin/dep"
-
-	export _RITA_SRC_DIR="$GOPATH/src/github.com/activecm/rita"
-	mkdir -p "$_RITA_SRC_DIR"
-
-	# Get the code from git since the build process is dependent on git
-	git clone http://github.com/activecm/rita "$_RITA_SRC_DIR" > /dev/null 2>&1
-
-	local old_dir="$PWD"
-	cd "$_RITA_SRC_DIR"
-	if [ -n "${_RITA_VERSION+x}" ]; then
-		git checkout $_RITA_VERSION > /dev/null 2>&1
-	fi
-	make > /dev/null
-	cd "$old_dir"
-}
-
 __install_rita() {
-	$_ELEVATE mkdir -p "$_CONFIG_PATH"
-	#$_ELEVATE mkdir -p "$_VAR_PATH"
-	$_ELEVATE mkdir -p "$_VAR_PATH/logs"
-	$_ELEVATE chmod 777 "$_VAR_PATH/logs"
-
-	$_ELEVATE mv -f "$_RITA_SRC_DIR/rita" "$_BIN_PATH/rita"
-	$_ELEVATE chown root:root "$_BIN_PATH/rita"
-	$_ELEVATE chmod 755 "$_BIN_PATH/rita"
-
-	$_ELEVATE cp -f "$_RITA_SRC_DIR/LICENSE" "$_CONFIG_PATH/LICENSE"
-	if [ -f "$_CONFIG_PATH/config.yaml" ]; then
-		printf "$_SUBITEM Backing up your current RITA config: $_CONFIG_PATH/config.yaml -> $_CONFIG_PATH/config.yaml.old \n"
-		$_ELEVATE mv -f "$_CONFIG_PATH/config.yaml" "$_CONFIG_PATH/config.yaml.old"
+	# Set _RITA_VERSION if it's not set
+	if [ -z ${_RITA_VERSION+x} ]; then
+		_LATEST_VERSION_COMMAND=\
+'curl -sSL https://api.github.com/repos/activecm/rita/releases/latest | grep tag_name | cut -d"\"" -f4'
+		_RITA_VERSION="$(eval $_LATEST_VERSION_COMMAND)"
 	fi
-	$_ELEVATE cp -f "$_RITA_SRC_DIR/etc/rita.yaml" "$_CONFIG_PATH/config.yaml"
+
+	_RITA_RELEASE_URL="https://github.com/activecm/rita/releases/download/$_RITA_VERSION"
+	_RITA_REPO_URL="https://raw.githubusercontent.com/activecm/rita/$_RITA_VERSION"
+	_RITA_BINARY_URL="$_RITA_RELEASE_URL/rita"
+	_RITA_CONFIG_URL="$_RITA_REPO_URL/etc/rita.yaml"
+	_RITA_LICENSE_URL="$_RITA_REPO_URL/LICENSE"
+	
+	_RITA_CONFIG_FILE="$_CONFIG_PATH/config.yaml"
+	_RITA_REINSTALL_CONFIG_FILE="$_CONFIG_PATH/config.yaml.new"
+
+	curl -sSL "$_RITA_BINARY_URL" -o "$_BIN_PATH/rita"
+	chmod 755 "$_BIN_PATH/rita"
+
+	mkdir -p "$_CONFIG_PATH"
+	mkdir -p "$_VAR_PATH"
+	mkdir -p "$_VAR_PATH/logs"
+	chmod 777 "$_VAR_PATH/logs"
+
+	curl -sSL "$_RITA_LICENSE_URL" -o "$_CONFIG_PATH/LICENSE"
+
+	if [ "$_REINSTALL_RITA" = "true" ]; then
+		# Don't overwrite existing config
+		curl -sSL "$_RITA_CONFIG_URL" -o "$_RITA_REINSTALL_CONFIG_FILE"
+		chmod 666 "$_RITA_REINSTALL_CONFIG_FILE"
+	else
+		curl -sSL "$_RITA_CONFIG_URL" -o "$_RITA_CONFIG_FILE"
+		chmod 666 "$_RITA_CONFIG_FILE"
+	fi
 
 	# All users can read and write rita's config file
-	$_ELEVATE chmod 755 "$_CONFIG_PATH"
-	$_ELEVATE chmod 666 "$_CONFIG_PATH/config.yaml"
+	chmod 755 "$_CONFIG_PATH"
 
-	$_ELEVATE touch "$_VAR_PATH/safebrowsing"
-	$_ELEVATE chmod 755 "$_VAR_PATH"
-	$_ELEVATE chmod 666 "$_VAR_PATH/safebrowsing"
+	touch "$_VAR_PATH/safebrowsing"
+	chmod 755 "$_VAR_PATH"
+	chmod 666 "$_VAR_PATH/safebrowsing"
 }
 
 # INFORMATION GATHERING
@@ -406,61 +305,6 @@ __gather_pkg_mgr() {
 	fi
 }
 
-__gather_go() {
-	_GO_PATH=""
-	_GO_INSTALLED_STD=false
-	if [ -f "/usr/local/go/bin/go" ]; then
-		_GO_INSTALLED_STD=true
-		_GO_PATH="/usr/local/go/bin"
-	fi
-
-	_GO_INSTALLED_NON_STD=false
-	if [ -n "$GOROOT" -a -f "$GOROOT/bin/go" ]; then
-		_GO_INSTALLED_NON_STD=true
-		_GO_PATH="$GOROOT/bin"
-	fi
-
-	_GO_INSTALLED=false
-	if [ $_GO_INSTALLED_STD = "true" -o $_GO_INSTALLED_NON_STD = "true" ]; then
-		_GO_INSTALLED=true
-	fi
-
-	_GO_OUT_OF_DATE=false
-	if [ $_GO_INSTALLED = "true" ]; then
-		case `$_GO_PATH/go version | awk '{print $3}'` in
-		go1|go1.2*|go1.3*|go1.4*|go1.5*|go1.6*|"")
-			_GO_OUT_OF_DATE=true
-			;;
-		esac
-	fi
-
-	_GO_PATH_SCRIPT="/etc/profile.d/go-path.sh"
-	_GO_PATH_SCRIPT_INSTALLED=false
-
-	if [ -f "$_GO_PATH_SCRIPT" ]; then
-		source "$_GO_PATH_SCRIPT"
-		_GO_PATH_SCRIPT_INSTALLED=true
-	fi
-
-	_GO_IN_PATH=false
-	if [ -n "$(type -fp go)" ]; then
-		_GO_IN_PATH=true
-	fi
-
-	_GOPATH_PATH_SCRIPT="$HOME/.gopath-path.sh"
-	_GOPATH_PATH_SCRIPT_INSTALLED=false
-
-	if [ -f "$_GOPATH_PATH_SCRIPT" ]; then
-		source "$_GOPATH_PATH_SCRIPT"
-		_GOPATH_PATH_SCRIPT_INSTALLED=true
-	fi
-
-	_GOPATH_EXISTS=false
-	if [ -n "$GOPATH" ]; then
-		_GOPATH_EXISTS=true
-	fi
-
-}
 
 __gather_bro() {
 	_BRO_PATH=""
@@ -514,12 +358,6 @@ __explain() {
 	printf "$_ITEM This installer will: \n"
 	if [ $_BRO_INSTALLED = "false" -a $_INSTALL_BRO = "true" ]; then
 		printf "$_SUBITEM Install Bro IDS to /opt/bro \n"
-	fi
-	if [ $_GO_INSTALLED = "false" ]; then
-		printf "$_SUBITEM Install Go to /usr/local/go \n"
-	fi
-	if [ $_GOPATH_EXISTS = "false" ]; then
-		printf "$_SUBITEM Create a Go build environment (GOPATH) in $HOME/go \n"
 	fi
 	if [ $_MONGO_INSTALLED = "false" -a $_INSTALL_MONGO = "true" ]; then
 		printf "$_SUBITEM Install MongoDB \n"
@@ -583,16 +421,16 @@ __install_packages() {
 					;;
 			esac
 		fi
-		eval $_ELEVATE $_PKG_INSTALL $pkg >/dev/null 2>&1
+		eval $_PKG_INSTALL $pkg >/dev/null 2>&1
 		shift
 	done
 }
 
 __freshen_packages() {
 	if [ $_PKG_MGR -eq 1 ]; then   #apt
-		$_ELEVATE apt-get -qq update > /dev/null 2>&1
+		apt-get -qq update > /dev/null 2>&1
 	elif [ $_PKG_MGR -eq 2 ]; then #yum
-		$_ELEVATE yum -q makecache > /dev/null 2>&1
+		yum -q makecache > /dev/null 2>&1
 	fi
 }
 
@@ -608,15 +446,15 @@ __package_installed() {
 __add_deb_repo() {
 	if [ ! -s "/etc/apt/sources.list.d/$2.list" ]; then
 		if [ ! -z "$3" ]; then
-			curl -s -L "$3" | $_ELEVATE apt-key add - > /dev/null 2>&1
+			curl -s -L "$3" | apt-key add - > /dev/null 2>&1
 		fi
-		echo "$1" | $_ELEVATE tee "/etc/apt/sources.list.d/$2.list" > /dev/null
+		echo "$1" | tee "/etc/apt/sources.list.d/$2.list" > /dev/null
 		__freshen_packages
 	fi
 }
 
 __add_rpm_repo() {
-	$_ELEVATE yum-config-manager -q --add-repo=$1 > /dev/null 2>&1
+	yum-config-manager -q --add-repo=$1 > /dev/null 2>&1
 	__freshen_packages
 }
 
