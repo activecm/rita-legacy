cd4dc959fce36c23882e3e0b6f37fc4023ac69c7
logan lembke <logan@blackhillsinfosec.com>
2018-05-21 11:09:07 -0600

    Make show ports and show human readable report mutually exclusive in show-scans

--- commands/show-scans.go
+++ commands/show-scans.go
@@ -22,7 +22,7 @@ func init() {
 			configFlag,
 			cli.BoolFlag{
 				Name:  "ports, P",
-				Usage: "Show which individual ports were scanned",
+				Usage: "Show which individual ports were scanned. Incompaitble with --human-readable.",
 			},
 		},
 		Action: func(c *cli.Context) error {
@@ -31,6 +31,10 @@ func init() {
 				return cli.NewExitError("Specify a database", -1)
 			}
 			showPorts := c.Bool("ports")
+			humanReadable := c.Bool("human-readable")
+			if showPorts && humanReadable {
+				return cli.NewExitError("--ports and --human-readable are incompatible", -1)
+			}
 
 			res := database.InitResources(c.String("config"))
 
@@ -42,8 +46,8 @@ func init() {
 				return cli.NewExitError("No results were found for "+db, -1)
 			}
 
-			if c.Bool("human-readable") {
-				err := showScansHuman(scans, showPorts)
+			if humanReadable {
+				err := showScansHuman(scans)
 				if err != nil {
 					return cli.NewExitError(err.Error(), -1)
 				}
@@ -87,28 +91,12 @@ func showScans(scans []scanning.Scan, showPorts bool) error {
 }
 
 // showScans prints all scans for a given database
-func showScansHuman(scans []scanning.Scan, showPorts bool) error {
+func showScansHuman(scans []scanning.Scan) error {
 	table := tablewriter.NewWriter(os.Stdout)
 	header := []string{"Source", "Destination", "Ports Scanned"}
-	if showPorts {
-		header = append(header, "Ports")
-		table.SetColWidth(80)
-	}
 	table.SetHeader(header)
 	for _, scan := range scans {
 		data := []string{scan.Src, scan.Dst, strconv.Itoa(scan.PortCount)}
-
-		if showPorts {
-			portSet := []byte("")
-			for i, port := range scan.PortSet {
-				if i != 0 {
-					portSet = append(portSet, " "...)
-				}
-				portSet = strconv.AppendInt(portSet, int64(port), 10)
-			}
-			data = append(data, string(portSet))
-		}
-
 		table.Append(data)
 	}
 	table.Render()
