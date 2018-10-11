3d2bd972725d1a42ebf475b622d7ab0ff1c97be3
zaowen <zacharyowen@acm.org>
2018-10-11 03:48:38 +0000

    Make requested changes

--- commands/update-check.go
+++ commands/update-check.go
@@ -2,11 +2,11 @@ package commands
 
 import (
 	"time"
-	"net/http"
-	"io/ioutil"
 	"strings"
 	"strconv"
+	"context"
 
+	"github.com/google/go-github/github"
 	"github.com/activecm/rita/resources"
 	"github.com/activecm/rita/config"
 	log "github.com/sirupsen/logrus"
@@ -22,74 +22,81 @@ func UpdateCheck( configFile string) string {
 		return ""
 	}
 
-	//Check Log for Versioning
+	//Check Logs for Versioning
 	m := res.MetaDB
-	t, s := m.LastCheck()
+	timestamp, version := m.LastCheck()
 
-	days := time.Now().Sub( t ).Hours()/24
+	days := time.Now().Sub( timestamp ).Hours()/24
 
 	if days > float64(delta) {
 
-		s = remoteVersion()
+		version, err := getRemoteVersion()
 
-		if s == "" {
+		if err == nil {
 			return ""
 		}
 
+		//Log checked version.
 		res.Log.WithFields(log.Fields{
 			"Message":  "Checking versions...",
 			"LastUpdateCheck": time.Now(),
-			"NewestVersion": s,
+			"NewestVersion": version,
 		}).Info("Checking for new version")
 	}
 
-	if ( config.Version != s ) {
-		return informUser( s )
+	if ( config.Version != version ) {
+		return informUser( version )
 	}
 
 	return ""
 }
 
+// Versions look like "v#.#.#". Returns just the numbers.
 func parseVersion( s string ) []string {
 	t := strings.Trim(s, "v\n")
 	return strings.Split( t, "." )
 }
 
-func remoteVersion() string {
-	resp, err := http.Get("https://raw.githubusercontent.com/zaowen/rita/UpdateCheck/Version")
-	if err != nil {
-		return ""
-	}
-	defer resp.Body.Close()
+func getRemoteVersion() (string, error){
+	client := github.NewClient(nil)
+	refs, _, err := client.Git.GetRefs( context.Background(), "activecm", "rita", "refs/tags/v")
 
-	version, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return ""
+	if err == nil {
+		s := strings.TrimPrefix( *refs[len(refs)-1].Ref, "refs/tags/")
+		return s, nil
+	} else {
+		return "", err
 	}
-	return strings.Trim(string(version), "\n")
 }
 
-func informUser( s string ) string {
+// Assembles a notice for the user informing them of an upgrade.
+// The return value is printed regardless so, "" is returned on errror.
+func informUser( verStr string ) string {
 	var ret string;
 	var versions [3]string;
 	versions[0] = "Major"
 	versions[1] = "Minor"
 	versions[2] = "Patch"
 
-	ps := parseVersion( s )
-	pv := parseVersion( config.Version )
+	parsedRemoteVers := parseVersion( verStr )
+	parsedLocalVers := parseVersion( config.Version )
 
 	ret += "\nTheres a new "
 
 	for i := 0; i < len(versions); i++ {
-		psi, _ := strconv.Atoi( ps[i] )
-		pvi, _ := strconv.Atoi( pv[i] )
-		if psi > pvi {
+		remote, err1 := strconv.Atoi( parsedRemoteVers[i] )
+		local, err2 := strconv.Atoi( parsedLocalVers[i] )
+
+		if err1 != nil || err2 != nil {
+			return ""
+		}
+
+		if  remote > local {
 			ret += versions[i] + " "
 			break
 		}
 	}
-	ret += "version of RITA " + s + " available at:\n"
+	ret += "version of RITA " + verStr + " available at:\n"
 	ret += "https://github.com/activecm/rita\n"
 	return ret
 }
