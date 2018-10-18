2ada4a2a12669dd86a740234e6e801139a38485f
meljbruno <meljbruno@gmail.com>
2018-10-18 11:54:56 -0400

    Fail build if static tests fail

--- static-tests.sh
+++ static-tests.sh
@@ -1,22 +1,26 @@
 #!/bin/sh
 
-echo "Running static test - gofmt\n"
+echo "Running static test - gofmt"
 GOFMT_FILES=$(gofmt -l .)
 if [ -n "${GOFMT_FILES}" ]; then
   printf >&2 'gofmt failed for the following files:\n%s\n\nplease run "gofmt -w ." on your changes before committing.\n' "${GOFMT_FILES}"
-  # exit 1
+  TEST_STATUS=FAIL
 fi
 
-echo "Running static test - golint\n"
+echo "Running static test - golint"
 GOLINT_ERRORS=$(golint ./... | grep -v "Id should be")
 if [ -n "${GOLINT_ERRORS}" ]; then
-  printf >&2 'golint failed for the following reasons:\n%s\n\nplease run 'golint ./...' on your changes before committing.\n' "${GOLINT_ERRORS}"
-  # exit 1
+  printf >&2 "golint failed for the following reasons:\n%s\n\nplease run 'golint ./...' on your changes before committing.\n" "${GOLINT_ERRORS}"
+  TEST_STATUS=FAIL
 fi
 
-echo "Running static test - go tool vet\n"
+echo "Running static test - go tool vet"
 GOVET_ERRORS=$(go tool vet $(find . -name '*.go' | grep -v '/vendor/') 2>&1)
 if [ -n "${GOVET_ERRORS}" ]; then
-  printf >&2 'go vet failed for the following reasons:\n%s\n\nplease run "go tool vet *.go" on your changes before committing.\n' "${GOVET_ERRORS}"
-  # exit 1
+  printf >&2 "go vet failed for the following reasons:\n%s\n\nplease run "go tool vet *.go" on your changes before committing.\n" "${GOVET_ERRORS}"
+  TEST_STATUS=FAIL
+fi
+
+if [ "$TEST_STATUS" = "FAIL" ]; then
+  exit 1
 fi
