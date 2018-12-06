440a675cc69da969d57c2620b5e00ec3af6586ea
Ethan Robish <ethan.robish@gmail.com>
2018-12-05 19:46:42 -0600

    Adding comments to aggregation pipeline

--- analysis/structure/uconn.go
+++ analysis/structure/uconn.go
@@ -64,9 +64,19 @@ func getUniqueConnectionsScript(conf *config.Config) (string, string, []mgo.Inde
 		{Key: []string{"connection_count"}},
 	}
 
-	// Aggregation script
+	// Aggregation to calculate various metrics (shown in the $project stage) that 
+	// occur between a unique IP pair. That is, all individual connections between two
+	// given IPs will be summarized into a single entry in the resulting uconn collection.
+	// We only process connections between IPs that cross the network border, 
+	// i.e. internal <-> external traffic.
+	// This is mainly for performance (not having to process int<->int or ext<->ext)
+	// but is also to reduce false positives as we are specifically looking for command
+	// & control channels where a compromised internal system is communicating with an 
+	// attacker's server on the internet.
 	pipeline := []bson.D{
 		{
+			// Only match on connections that are internal->external or external->internal
+			// i.e. Exclude anything internal<->internal or external<->external
 			{"$match", bson.M{
 				"$or": []bson.M{
 					bson.M{
@@ -85,14 +95,20 @@ func getUniqueConnectionsScript(conf *config.Config) (string, string, []mgo.Inde
 		{
 			{"$group", bson.D{
 				{"_id", bson.D{
+					// Each of these should be a static value per
+					// entry in the collection. In addition to defining
+					// the entry's key, putting them here makes them available
+					// for storing in the $project stage through $_id.*
 					{"src", "$id_orig_h"},
 					{"dst", "$id_resp_h"},
 					{"ls", "$local_orig"},
 					{"ld", "$local_resp"},
 				}},
+				// Total number of connections between two hosts
 				{"conns", bson.D{
 					{"$sum", 1},
 				}},
+				// Total number of bytes sent back and forth
 				{"tbytes", bson.D{
 					{"$sum", bson.D{
 						{"$add", []interface{}{
@@ -101,6 +117,7 @@ func getUniqueConnectionsScript(conf *config.Config) (string, string, []mgo.Inde
 						}},
 					}},
 				}},
+				// Average number of bytes sent back and forth
 				{"abytes", bson.D{
 					{"$avg", bson.D{
 						{"$add", []interface{}{
@@ -109,10 +126,13 @@ func getUniqueConnectionsScript(conf *config.Config) (string, string, []mgo.Inde
 						}},
 					}},
 				}},
+				// Total sum of all connection durations
 				{"tdur", bson.D{
 					{"$sum", "$duration"},
 				}},
+				// Array of all connection timestamps
 				{"ts", bson.D{{"$push", "$ts"}}},
+				// Array of bytes sent from origin in each connection
 				{"orig_bytes", bson.D{{"$push", "$orig_ip_bytes"}}},
 			}},
 		},
@@ -138,64 +158,3 @@ func getUniqueConnectionsScript(conf *config.Config) (string, string, []mgo.Inde
 
 	return sourceCollectionName, newCollectionName, keys, pipeline
 }
-
-// pipeline := []bson.D{
-// 	{
-// 		{"$group", bson.D{
-// 			{"_id", bson.D{
-// 				{"src", "$id_orig_h"},
-// 				{"dst", "$id_resp_h"},
-// 			}},
-// 			{"connection_count", bson.D{
-// 				{"$sum", 1},
-// 			}},
-// 			{"src", bson.D{
-// 				{"$first", "$id_orig_h"},
-// 			}},
-// 			{"dst", bson.D{
-// 				{"$first", "$id_resp_h"},
-// 			}},
-// 			{"local_src", bson.D{
-// 				{"$first", "$local_orig"},
-// 			}},
-// 			{"local_dst", bson.D{
-// 				{"$first", "$local_resp"},
-// 			}},
-// 			{"total_bytes", bson.D{
-// 				{"$sum", bson.D{
-// 					{"$add", []interface{}{
-// 						"$orig_ip_bytes",
-// 						"$resp_ip_bytes",
-// 					}},
-// 				}},
-// 			}},
-// 			{"avg_bytes", bson.D{
-// 				{"$avg", bson.D{
-// 					{"$add", []interface{}{
-// 						"$orig_ip_bytes",
-// 						"$resp_ip_bytes",
-// 					}},
-// 				}},
-// 			}},
-// 			{"total_duration", bson.D{
-// 				{"$sum", "$duration"},
-// 			}},
-// 		}},
-// 	},
-// 	{
-// 		{"$project", bson.D{
-// 			{"_id", 0},
-// 			{"connection_count", 1},
-// 			{"src", 1},
-// 			{"dst", 1},
-// 			{"local_src", 1},
-// 			{"local_dst", 1},
-// 			{"total_bytes", 1},
-// 			{"avg_bytes", 1},
-// 			{"total_duration", 1},
-// 		}},
-// 	},
-// 	{
-// 		{"$out", newCollectionName},
-// 	},
-// }
