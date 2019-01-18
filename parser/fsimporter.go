fe7ff2d35fdd8b53302b7d414de28deb09cef4cd
Ethan Robish <ethan.robish@gmail.com>
2019-01-18 11:48:02 -0600

    Setting local values based on InternalSubnets

--- parser/fsimporter.go
+++ parser/fsimporter.go
@@ -257,6 +257,11 @@ func (fs *FSImporter) parseFiles(indexedFiles []*fpt.IndexedFile, parsingThreads
 
 							// If connection pair is not subject to filtering, process
 							if !ignore {
+								// Override LocalOrigin and LocalResponse fields based on InternalSubnets setting
+								// Changes to parseConn are also made in the data variable
+								parseConn.FieldByName("LocalOrigin").SetBool(containsIP(fs.GetInternalSubnets(), net.ParseIP(uconn.src)))
+								parseConn.FieldByName("LocalResponse").SetBool(containsIP(fs.GetInternalSubnets(), net.ParseIP(uconn.dst)))
+
 								// Safely store the number of conns for this uconn
 								mutex.Lock()
 								connMap[uconn] = connMap[uconn] + 1
