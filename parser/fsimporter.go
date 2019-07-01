459741284cc26e12159441df512b300b1dcc7965
SamuelCarroll <carroll.samuel.jacob@gmail.com>
2019-07-01 12:58:38 -0600

    Adding an additional warning if no uconn data found (#476)

--- parser/fsimporter.go
+++ parser/fsimporter.go
@@ -769,6 +769,8 @@ func (fs *FSImporter) buildUconns(uconnMap map[string]*uconn.Pair) {
 		uconnRepo.Upsert(uconnMap)
 	} else {
 		fmt.Println("\t[!] No Uconn data to analyze")
+		fmt.Printf("\t\t[!!] No local network traffic found, please check ")
+		fmt.Println("InternalSubnets in your RTIA config (/etc/rita/config.yaml)")
 	}
 
 }
@@ -787,6 +789,8 @@ func (fs *FSImporter) buildHosts(hostMap map[string]*host.IP) {
 		hostRepo.Upsert(hostMap)
 	} else {
 		fmt.Println("\t[!] No Host data to analyze")
+		fmt.Printf("\t\t[!!] No local network traffic found, please check ")
+		fmt.Println("InternalSubnets in your RTIA config (/etc/rita/config.yaml)")
 	}
 }
 
