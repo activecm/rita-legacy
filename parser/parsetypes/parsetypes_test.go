29b7d983cfe8ec29a972ebf8659077e8599f7bb1
Ethan Robish <ethan.robish@gmail.com>
2019-11-20 17:04:01 -0600

    Support multiple time formats

--- parser/parsetypes/parsetypes_test.go
+++ parser/parsetypes/parsetypes_test.go
@@ -18,3 +18,22 @@ func TestNewBroDataFactory(t *testing.T) {
 		}
 	}
 }
+
+func TestConvertTimestamp(t *testing.T) {
+	testCases := []struct {
+		input interface{}
+		expected int64
+	}{
+		{1517336042.090842, 1517336042},
+		{1517336042, 1517336042},
+		{"2018-01-30T18:14:02Z", 1517336042},
+		{0, 0},
+		{"", 0},
+		{nil, 0},
+	}
+
+	for _, testCase := range testCases {
+		actual := convertTimestamp(testCase.input)
+		require.Equal(t, testCase.expected, actual, "input: %v", testCase.input)
+	}
+}
