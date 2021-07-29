42bda483f6183f3ff65bad7d74b32d9cd4c3454e
Logan L <logan@bhis.co>
2021-07-29 17:10:33 -0600

    Replace calls to mgo query .All and len() with .Count() which avoids uncessary deserialization in beacon pkg

--- pkg/beacon/mongodb.go
+++ pkg/beacon/mongodb.go
@@ -80,6 +80,7 @@ func (r *repo) Upsert(uconnMap map[string]*uconn.Input) {
 		r.res.Config.S.Rolling.CurrentChunk,
 		r.res.DB,
 		r.res.Config,
+		r.res.Log,
 		writerWorker.collect,
 		writerWorker.close,
 	)
