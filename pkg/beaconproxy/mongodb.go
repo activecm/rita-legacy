9340d4b87ea81542906451164a72def7c991fbae
fullmetalcache <fullmetalcache@gmail.com>
2021-08-18 09:00:58 -0600

    Update indexes

--- pkg/beaconproxy/mongodb.go
+++ pkg/beaconproxy/mongodb.go
@@ -47,10 +47,12 @@ func (r *repo) CreateIndexes() error {
 	// set desired indexes
 	indexes := []mgo.Index{
 		{Key: []string{"-score"}},
+		{Key: []string{"src", "dst", "fqdn", "src_network_uuid", "dst_network_uuid"}, Unique: true},
 		{Key: []string{"dst", "dst_network_uuid"}},
 		{Key: []string{"src", "src_network_uuid"}},
 		{Key: []string{"fqdn"}},
 		{Key: []string{"-connection_count"}},
+		{Key: []string{"proxy.ip", "proxy.network_uuid"}},
 	}
 
 	// create collection
