19c729ce07960e2b158f6ac4556e4b9996e5e9bb
Logan L <logan@bhis.co>
2022-06-30 18:19:17 -0600

    Create host summary entries for dat.mbsni

--- pkg/beaconsni/mongodb.go
+++ pkg/beaconsni/mongodb.go
@@ -143,50 +143,50 @@ func (r *repo) Upsert(tlsMap map[string]*sniconn.TLSInput, httpMap map[string]*s
 
 	// // Phase 2: Summary
 
-	// // initialize a new writer for the summarizer
-	// writerWorker = newMgoBulkWriter(r.database, r.config, r.log, "beacon")
-	// summarizerWorker := newSummarizer(
-	// 	r.config.S.Rolling.CurrentChunk,
-	// 	r.database,
-	// 	r.config,
-	// 	r.log,
-	// 	writerWorker.collect,
-	// 	writerWorker.close,
-	// )
+	// initialize a new writer for the summarizer
+	writerWorker = newMgoBulkWriter(r.database, r.config, r.log, "beaconSNI")
+	summarizerWorker := newSummarizer(
+		r.config.S.Rolling.CurrentChunk,
+		r.database,
+		r.config,
+		r.log,
+		writerWorker.collect,
+		writerWorker.close,
+	)
 
-	// // kick off the threaded goroutines
-	// for i := 0; i < util.Max(1, runtime.NumCPU()/2); i++ {
-	// 	summarizerWorker.start()
-	// 	writerWorker.start()
-	// }
+	// kick off the threaded goroutines
+	for i := 0; i < util.Max(1, runtime.NumCPU()/2); i++ {
+		summarizerWorker.start()
+		writerWorker.start()
+	}
 
-	// // grab the local hosts we have seen during the current analysis period
-	// // get local hosts only for the summary
-	// var localHosts []data.UniqueIP
-	// for _, entry := range hostMap {
-	// 	if entry.IsLocal {
-	// 		localHosts = append(localHosts, entry.Host)
-	// 	}
-	// }
+	// grab the local hosts we have seen during the current analysis period
+	// get local hosts only for the summary
+	var localHosts []data.UniqueIP
+	for _, entry := range hostMap {
+		if entry.IsLocal {
+			localHosts = append(localHosts, entry.Host)
+		}
+	}
 
-	// // add a progress bar for troubleshooting
-	// p = mpb.New(mpb.WithWidth(20))
-	// bar = p.AddBar(int64(len(localHosts)),
-	// 	mpb.PrependDecorators(
-	// 		decor.Name("\t[-] Beacon Analysis (2/2):", decor.WC{W: 30, C: decor.DidentRight}),
-	// 		decor.CountersNoUnit(" %d / %d ", decor.WCSyncWidth),
-	// 	),
-	// 	mpb.AppendDecorators(decor.Percentage()),
-	// )
+	// add a progress bar for troubleshooting
+	p = mpb.New(mpb.WithWidth(20))
+	bar = p.AddBar(int64(len(localHosts)),
+		mpb.PrependDecorators(
+			decor.Name("\t[-] SNI Beacon Aggregation:", decor.WC{W: 30, C: decor.DidentRight}),
+			decor.CountersNoUnit(" %d / %d ", decor.WCSyncWidth),
+		),
+		mpb.AppendDecorators(decor.Percentage()),
+	)
 
-	// // loop over the local hosts that need to be summarized
-	// for _, localHost := range localHosts {
-	// 	summarizerWorker.collect(localHost)
-	// 	bar.IncrBy(1)
-	// }
+	// loop over the local hosts that need to be summarized
+	for _, localHost := range localHosts {
+		summarizerWorker.collect(localHost)
+		bar.IncrBy(1)
+	}
 
-	// p.Wait()
+	p.Wait()
 
-	// // start the closing cascade (this will also close the other channels)
-	// summarizerWorker.close()
+	// start the closing cascade (this will also close the other channels)
+	summarizerWorker.close()
 }
