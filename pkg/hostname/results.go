4830c397b4888a840aa2699a4d63c9e771176181
Kirk Hauck <kirkihauck@gmail.com>
2023-07-18 15:08:43 -0600

    Setup FQDN query

--- pkg/hostname/results.go
+++ pkg/hostname/results.go
@@ -4,6 +4,7 @@ import (
 	"github.com/activecm/rita/pkg/data"
 	"github.com/activecm/rita/resources"
 	"github.com/globalsign/mgo/bson"
+	"github.com/pkg/hostname"
 )
 
 // IPResults returns the IP addresses the hostname was seen resolving to in the dataset
@@ -40,5 +41,27 @@ func IPResults(res *resources.Resources, hostname string) ([]data.UniqueIP, erro
 
 	var ipResults []data.UniqueIP
 	err := ssn.DB(res.DB.GetSelectedDB()).C(res.Config.T.DNS.HostnamesTable).Pipe(ipsForHostnameQuery).AllowDiskUse().All(&ipResults)
+	return ipResults, err
+}
+
+// FQDNResults returns the FQDNs the IP address was seen resolving to in the dataset
+func FQDNResults(res *resources.Resources, hostname string) ([]hostname.FQDNResult, error) {
+	ssn := res.DB.Session.Copy()
+	defer ssn.Close()
+
+	ipPattern := `^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$|^(?:[A-F0-9]{1,4}:){7}[A-F0-9]{1,4}$
+	`
+	fqdnsForHostnameQuery := []bson.M{
+		{"$match": bson.M{
+			"dat.ips.ip": bson.RegEx{Pattern: ipPattern},
+		}},
+		{"$group": bson.M{
+			"_id": "$host",
+		}},
+		
+	}
+
+	var ipResults []data.UniqueIP
+	err := ssn.DB(res.DB.GetSelectedDB()).C(res.Config.T.DNS.HostnamesTable).Pipe(fqdnsForHostnameQuery).AllowDiskUse().All(&ipResults)
 	return ipResults, err
 }
